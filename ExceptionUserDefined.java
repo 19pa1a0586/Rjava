254f4efea2a265ae7941fc839af0a3df59ac0cd3
19pa1a0586 <60594951+19pa1a0586@users.noreply.github.com>
2021-02-23 23:59:16 +0530

    Add files via upload

---
+++ ExceptionUserDefined.java
@@ -0,0 +1,42 @@
+
+package exception.user.defined;
+
+import java.util.Scanner;
+
+class InvalidNumberException extends Exception
+{
+    public String toString()
+    {
+        return "We must enter Numbers in between 1 and 9.";
+    }
+}
+public class ExceptionUserDefined {
+    
+    public static int number() throws InvalidNumberException
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number from 1 to 9 : ");
+        int num = in.nextInt();
+        if(num <= 0 || num >= 10)
+        {
+            throw new InvalidNumberException();
+        }
+        return num;
+    }
+    
+    public static void numberCall() throws InvalidNumberException
+    {
+        System.out.println("Number is : "+number());
+    }
+    public static void main(String[] args) {
+        try
+        {
+            numberCall();
+        }
+        catch(InvalidNumberException e)
+        {
+            System.out.println("Exception in main method is : "+e);
+        }
+    }
+    
+}
