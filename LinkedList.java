3f20de70682811b2cdfddaf2ec37196a4f21b1c6
19pa1a0586 <60594951+19pa1a0586@users.noreply.github.com>
2021-03-03 15:51:48 +0530

    Add files via upload

---
+++ LinkedList.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner in = new Scanner(System.in);
+	    System.out.print("Enter the size of the Array : ");
+	    int n = in.nextInt();
+	    LinkedList<Integer> arr = new LinkedList<>();
+	    for(int i = 0; i < n; i++)
+	    {
+	        arr.add(in.nextInt());
+	    }
+        System.out.println("Array List is : "+arr);
+        arr.remove(1);
+        System.out.println("Array List after remove is : "+arr);
+        System.out.println("Element at Index 1 is : "+arr.get(1));
+        System.out.println("Element at Index 2 is replaced with 5 and the element that is removed : "+arr.set(2,5));
+        System.out.println("Array List after replace is : "+arr);
+	}
+}
+
+//Routput:
+
+// Enter the size of the Array : 5                                                                                                 
+// 9 8 3 4 5                                                                                                                       
+// Array List is : [9, 8, 3, 4, 5]                                                                                                 
+// Array List after remove is : [9, 3, 4, 5]                                                                                       
+// Element at Index 1 is : 3                                                                                                       
+// Element at Index 2 is replaced with 5 and the element that is removed : 4                                                       
+// Array List after replace is : [9, 3, 5, 5] 
