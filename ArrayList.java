ed0e80662abd7150b0d549f636ffd61a750d5e5f
19pa1a0586 <60594951+19pa1a0586@users.noreply.github.com>
2021-03-03 15:15:56 +0530

    Update ArrayList.java

--- ArrayList.java
+++ ArrayList.java
@@ -10,12 +10,12 @@ public class ArrayList
 	    {
 	        arr.add(in.nextInt());
 	    }
-        System.out.println("Array List is : "+arr);
-        arr.remove(1);
-        System.out.println("Array List after remove is : "+arr);
-        System.out.println("Element at Index 1 is : "+arr.get(1));
-        System.out.println("Element at Index 2 is replaced with 5 and the element that is removed : "+arr.set(2,5));
-        System.out.println("Array List after replace is : "+arr);
+            System.out.println("Array List is : "+arr);
+            arr.remove(1);
+            System.out.println("Array List after remove is : "+arr);
+            System.out.println("Element at Index 1 is : "+arr.get(1));
+            System.out.println("Element at Index 2 is replaced with 5 and the element that is removed : "+arr.set(2,5));
+            System.out.println("Array List after replace is : "+arr);
 	}
 }
 
